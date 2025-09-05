52420ea059f664b2a55bb941f10b3d8728e9c78c
Mikhail Mayorov <mayorov.m.a@ya.ru>
2025-09-05 13:34:07 +0400

    Send rodata to Lean.

--- test_utils/src/lib.rs
+++ test_utils/src/lib.rs
@@ -337,8 +337,9 @@ macro_rules! test_interpreter_and_jit {
                 |(key, (symbol, _))|
                     lean_impl::FunctionRegistryEntry { key : key, symbol : symbol.to_vec(), address : "0".to_string() }
                 ).collect();
-            lean_vm_state = lean_impl::lean_test(code_bytes.to_vec(), $mem.to_vec(),
-                code_vaddr as u64, $executable.get_entrypoint_instruction_offset() as u64, version_num, context_object.remaining as u64,
+            lean_vm_state = lean_impl::lean_test(code_bytes.to_vec(), $mem.to_vec(), $executable.get_ro_section().to_vec(),
+                code_vaddr as u64, $executable.get_ro_region().vm_addr, $executable.get_entrypoint_instruction_offset() as u64,
+                version_num, context_object.remaining as u64,
                 $executable.get_config().max_call_depth as u64, $executable.get_config().stack_size() as u64,
                 function_registry_vec, loader_function_registry_vec);
         }
